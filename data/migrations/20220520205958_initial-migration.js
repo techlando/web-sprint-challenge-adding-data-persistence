0c5537d19837038bb8ff7642250236929ef4e19d
Landon Phillips <landonphillips@Landons-MacBook-Pro.local>
2022-05-20 16:43:19 -0500

    wip

--- data/migrations/20220520205958_initial-migration.js
+++ data/migrations/20220520205958_initial-migration.js
@@ -2,16 +2,40 @@
 exports.up = async function(knex) {
   await knex.schema
     .createTable('projects', tbl => {
-        tbl.increments()
+        tbl.increments('project_id')
+        tbl.string('project_name', 121).notNullable()
+        tbl.string('project_description', 200)
+        tbl.boolean('project_completed').notNullable().defaultTo(0)
     })
     .createTable('resources', tbl => {
-        tbl.increments()
+        tbl.increments('resource_id')
+        tbl.string('resource_name', 132).notNullable().unique()
+        tbl.text('resource_description', 200)
     })
     .createTable('tasks', tbl => {
-        tbl.increments()
+        tbl.increments('task_id')
+        tbl.string('task_description', 200).notNullable()
+        tbl.text('task_notes', 200)
+        tbl.boolean('task_completed').notNullable().defaultTo(0)
+        tbl.integer('project_id')
+        .unsigned()
+        .notNullable()
+        .references('project_id')
+        .inTable('projects')
+        
     })
     .createTable('project_resources', tbl => {
-        tbl.increments()
+        tbl.increments('project_resource_id')
+        tbl.integer('project_id')
+        .unsigned()
+        .notNullable()
+        .references('project_id')
+        .inTable('projects')
+        tbl.integer('resource_id')
+        .unsigned()
+        .notNullable()
+        .references('resource_id')
+        .inTable('resources')
     })
 };
 
